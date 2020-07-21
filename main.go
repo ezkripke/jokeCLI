ee68f1c1b4e6bedd69b6384a19b77bf090344731
Ethan Kripke <ezkripke@college.harvard.edu>
2020-07-20 20:19:05 -0400

    working prototype

--- main.go
+++ main.go
@@ -1,9 +1,30 @@
 package main
 
 import (
-    "fmt"
+	"flag"
+	"fmt"
+	"github.com/ezkripke/jokeCLI/client"
 )
 
 func main() {
-    fmt.Println("stub")
+	// flags
+	c := flag.String(
+		"c",
+		string(client.DefaultCategory),
+		"Joke category (Any, Miscellaneous, Programming, Dark)",
+	)
+	t := flag.String(
+		"t",
+		string(client.DefaultType),
+		"Joke Type (Single, Twopart)",
+	)
+	flag.Parse()
+
+	jokeAPIclient := client.NewJokeAPIClient()
+
+	joke, err := jokeAPIclient.Fetch(client.JokeCategory(*c), client.JokeType(*t))
+	if err != nil {
+		fmt.Printf("An error occured fetching your comic:\n%s\n", err)
+	}
+	fmt.Println(joke.Fulljoke)
 }
